015325cd3e09abcfa8980507473a8671be4a23f3
Geri Adam Saputra <geriadam04@gmail.com>
2023-01-07 16:58:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex h-full flex-col">
+        <div className="mx-auto flex w-full grow flex-col px-6 pt-2 pb-8 md:max-w-7xl lg:px-8 short:pb-2 short:pt-2">
+          Hello World
+        </div>
+      </div>
     </div>
   );
 }
